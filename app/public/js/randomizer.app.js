585463fc5f61764f5f545d50c034490c82577220
pshusang <38292263+pshusang@users.noreply.github.com>
2019-09-15 18:09:48 -0400

    Fix the data for the profile

--- app/public/js/randomizer.app.js
+++ app/public/js/randomizer.app.js
@@ -1,17 +1,19 @@
 var randomizerApp = new Vue({
-  el#: '#randomizerMemberApp',
+  el: '#randomizerMemberApp',
   data: {
     members: []
   },
     methods: {
       fetchMembers()
       {
-        fetch('https://randomuser.me/api')
+        fetch('https://randomuser.me/api/?exc=registered')
         .then(response => response.json())
-        .then(json => {randomizerApp.members = json})
+        // .then(json => {randomizerApp.members = json.results[0]})
+        .then(function(json) {randomizerApp.members = json.results[0];
+        console.log(randomizerApp.members);})
       }
 },
-  create(){
+  created(){
     this.fetchMembers()
   }
-)};
+});
