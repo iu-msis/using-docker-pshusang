839cb8ef1bcdec0143da401dde040f441e9fe0ac
pshusang <38292263+pshusang@users.noreply.github.com>
2019-09-15 22:17:29 -0400

    Change the path for docker-compose and dockerfile

--- app/public/js/randomizer.app.js
+++ app/public/js/randomizer.app.js
@@ -1,7 +1,66 @@
 var randomizerApp = new Vue({
   el: '#randomizerMemberApp',
   data: {
-    members: []
+    members: [{
+      "gender": "",
+      "name": {
+        "title": "",
+        "first": "",
+        "last": ""
+      },
+      "location": {
+        "street": "",
+        "city": "",
+        "state": "",
+        "postcode": "",
+        "coordinates": {
+          "latitude": "",
+          "longitude": ""
+        },
+        "timezone": {
+          "offset": "",
+          "description": ""
+        }
+      },
+      "email": "",
+      "login": {
+        "uuid": "",
+        "username": "",
+        "password": "",
+        "salt": "",
+        "md5": "",
+        "sha1": "",
+        "sha256": ""
+      },
+      "dob": {
+        "date": "",
+        "age": null
+      },
+      "registered": {
+        "date": "",
+        "age": null
+      },
+      "phone": "",
+      "cell": "",
+      "id": {
+        "name": "",
+        "value": ""
+      },
+      "picture": {
+        "large": "",
+        "medium": "",
+        "thumbnail": ""
+      },
+      "nat": ""
+    }
+  ],
+  "info":
+    {
+    "seed": "",
+    "results": null,
+    "page": null,
+    "version": ""
+    }
   },
     methods: {
       fetchMembers()
